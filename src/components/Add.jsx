ffe0072b4d921571cb319edf7de64fd2668463bb
selajdin89 <selajdinmemeti89@gmail.com>
2022-08-16 23:13:00 +0200

    Update

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -42,7 +42,7 @@ function Add() {
         sx={{
           position: "fixed",
           bottom: 20,
-          left: { xs: "calc(50% -25)", md: 30 },
+          left: { xs: "calc(50% -28px)", md: 30 },
         }}
       >
         <Fab color="primary" aria-label="add">
